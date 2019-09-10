793826d8dce729a0115d2c9347bc0845659edffc
hcl2020 <907953320@qq.com>
2019-09-11 00:11:19 +0800

    feat: echarts map demo

--- client/src/components/Demo.js
+++ client/src/components/Demo.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import echarts from 'echarts';
+import 'echarts/extension/bmap/bmap';
+
 import './Demo.css';
+import { city, geoCoordMap } from './data';
 
 export default class Demo extends React.Component {
   constructor(props) {
@@ -10,7 +13,8 @@ export default class Demo extends React.Component {
   componentDidMount() {
     console.log('componentDidMount', this);
     this.timerID = setInterval(() => this.tick(), 1000);
-    this.initChart();
+    // this.initChart();
+    this.initChartMap();
   }
 
   componentWillUnmount() {
@@ -25,7 +29,7 @@ export default class Demo extends React.Component {
   }
 
   initChart() {
-    var myChart = echarts.init(this.refs.chart);
+    let myChart = echarts.init(this.refs.chart);
     // 绘制图表
     myChart.setOption({
       title: {
@@ -46,12 +50,292 @@ export default class Demo extends React.Component {
     });
   }
 
+  initChartMap() {
+    let data = city;
+
+    let convertData = function(data) {
+      let res = [];
+      for (let i = 0; i < data.length; i++) {
+        let geoCoord = geoCoordMap[data[i].name];
+        if (geoCoord) {
+          res.push({
+            name: data[i].name,
+            value: geoCoord.concat(data[i].value)
+          });
+        }
+      }
+      return res;
+    };
+
+    function renderItem(params, api) {
+      let coords = [
+        [116.7, 39.53],
+        [103.73, 36.03],
+        [112.91, 27.87],
+        [120.65, 28.01],
+        [119.57, 39.95]
+      ];
+      let points = [];
+      for (let i = 0; i < coords.length; i++) {
+        points.push(api.coord(coords[i]));
+      }
+      let color = api.visual('color');
+
+      return {
+        type: 'polygon',
+        shape: {
+          points: echarts.graphic.clipPointsByRect(points, {
+            x: params.coordSys.x,
+            y: params.coordSys.y,
+            width: params.coordSys.width,
+            height: params.coordSys.height
+          })
+        },
+        style: api.style({
+          fill: color,
+          stroke: echarts.color.lift(color)
+        })
+      };
+    }
+    let option = {
+      backgroundColor: 'transparent',
+      title: {
+        text: '全国主要城市空气质量',
+        subtext: 'data from PM25.in',
+        sublink: 'http://www.pm25.in',
+        left: 'center',
+        textStyle: {
+          color: '#fff'
+        }
+      },
+      tooltip: {
+        trigger: 'item'
+      },
+      bmap: {
+        center: [104.114129, 37.550339],
+        zoom: 5,
+        roam: true,
+        mapStyle: {
+          styleJson: [
+            {
+              featureType: 'water',
+              elementType: 'all',
+              stylers: {
+                color: '#044161'
+              }
+            },
+            {
+              featureType: 'land',
+              elementType: 'all',
+              stylers: {
+                color: '#004981'
+              }
+            },
+            {
+              featureType: 'boundary',
+              elementType: 'geometry',
+              stylers: {
+                color: '#064f85'
+              }
+            },
+            {
+              featureType: 'railway',
+              elementType: 'all',
+              stylers: {
+                visibility: 'off'
+              }
+            },
+            {
+              featureType: 'highway',
+              elementType: 'geometry',
+              stylers: {
+                color: '#004981'
+              }
+            },
+            {
+              featureType: 'highway',
+              elementType: 'geometry.fill',
+              stylers: {
+                color: '#005b96',
+                lightness: 1
+              }
+            },
+            {
+              featureType: 'highway',
+              elementType: 'labels',
+              stylers: {
+                visibility: 'off'
+              }
+            },
+            {
+              featureType: 'arterial',
+              elementType: 'geometry',
+              stylers: {
+                color: '#004981'
+              }
+            },
+            {
+              featureType: 'arterial',
+              elementType: 'geometry.fill',
+              stylers: {
+                color: '#00508b'
+              }
+            },
+            {
+              featureType: 'poi',
+              elementType: 'all',
+              stylers: {
+                visibility: 'off'
+              }
+            },
+            {
+              featureType: 'green',
+              elementType: 'all',
+              stylers: {
+                color: '#056197',
+                visibility: 'off'
+              }
+            },
+            {
+              featureType: 'subway',
+              elementType: 'all',
+              stylers: {
+                visibility: 'off'
+              }
+            },
+            {
+              featureType: 'manmade',
+              elementType: 'all',
+              stylers: {
+                visibility: 'off'
+              }
+            },
+            {
+              featureType: 'local',
+              elementType: 'all',
+              stylers: {
+                visibility: 'off'
+              }
+            },
+            {
+              featureType: 'arterial',
+              elementType: 'labels',
+              stylers: {
+                visibility: 'off'
+              }
+            },
+            {
+              featureType: 'boundary',
+              elementType: 'geometry.fill',
+              stylers: {
+                color: '#029fd4'
+              }
+            },
+            {
+              featureType: 'building',
+              elementType: 'all',
+              stylers: {
+                color: '#1a5787'
+              }
+            },
+            {
+              featureType: 'label',
+              elementType: 'all',
+              stylers: {
+                visibility: 'off'
+              }
+            }
+          ]
+        }
+      },
+      series: [
+        {
+          name: 'pm2.5',
+          type: 'scatter',
+          coordinateSystem: 'bmap',
+          data: convertData(data),
+          symbolSize: function(val) {
+            return val[2] / 10;
+          },
+          label: {
+            normal: {
+              formatter: '{b}',
+              position: 'right',
+              show: false
+            },
+            emphasis: {
+              show: true
+            }
+          },
+          itemStyle: {
+            normal: {
+              color: '#ddb926'
+            }
+          }
+        },
+        {
+          name: 'Top 5',
+          type: 'effectScatter',
+          coordinateSystem: 'bmap',
+          data: convertData(
+            data
+              .sort(function(a, b) {
+                return b.value - a.value;
+              })
+              .slice(0, 6)
+          ),
+          symbolSize: function(val) {
+            return val[2] / 10;
+          },
+          showEffectOn: 'emphasis',
+          rippleEffect: {
+            brushType: 'stroke'
+          },
+          hoverAnimation: true,
+          label: {
+            normal: {
+              formatter: '{b}',
+              position: 'right',
+              show: true
+            }
+          },
+          itemStyle: {
+            normal: {
+              color: '#f4e925',
+              shadowBlur: 10,
+              shadowColor: '#333'
+            }
+          },
+          zlevel: 1
+        },
+        {
+          type: 'custom',
+          coordinateSystem: 'bmap',
+          renderItem: renderItem,
+          itemStyle: {
+            normal: {
+              opacity: 0.5
+            }
+          },
+          animation: false,
+          silent: true,
+          data: [0],
+          z: -10
+        }
+      ]
+    };
+    let myChart = echarts.init(this.refs.chart);
+
+    myChart.setOption(option);
+  }
+
   render() {
     return (
       <div className="Demo">
-        <h1>Hello, {this.props.name}</h1>
-        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-        <div ref="chart" style={{ height: '300px' }}></div>
+        <h1>
+          Hello, {this.props.name}, It is {this.state.date.toLocaleTimeString()}
+        </h1>
+        <div ref="chart" style={{ height: '600px' }}></div>
       </div>
     );
   }
