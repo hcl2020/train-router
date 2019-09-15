f0962dbc3b8df55c95380186dbe70ae832022d80
hcl2020 <907953320@qq.com>
2019-09-15 17:41:42 +0800

    chore:  移除demo代码

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
-// import Demo from './components/Demo';
 import Map from './components/Map';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      {/* <Demo name="demo"></Demo> */}
-      <Map name="Map"></Map>
+    <div className='App'>
+      <Map name='Map'></Map>
     </div>
   );
 }
